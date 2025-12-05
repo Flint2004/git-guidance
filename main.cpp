f793ca6e7ccfa7aedd106b52374f3fd90486261f
YourName <YourEmail@example.com>
2025-12-05 22:39:33 +0800

    Add greet function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void greet() {
+    cout << "Welcome to Git version control!" << endl;
+}
+
 int main() {
     cout << "Hello, Git!" << endl;
+    greet();
     return 0;
 }
 
